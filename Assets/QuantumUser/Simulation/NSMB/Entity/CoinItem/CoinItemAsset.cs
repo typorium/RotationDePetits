5d671e45919daf7234a77a0c10ddf34674481a0b
Amy54Desu <69287652+Amy54Desu@users.noreply.github.com>
2026-02-19 03:41:38 +0900

    bruh I forGOt to press Ctrl + S
    the commit speaks for itself. BigPowerUP and VerticalPowerUP is kept in case they're needed for whatever reason even though the Stage Banned PowerUP list is better. meow

--- Assets/QuantumUser/Simulation/NSMB/Entity/CoinItem/CoinItemAsset.cs
+++ Assets/QuantumUser/Simulation/NSMB/Entity/CoinItem/CoinItemAsset.cs
@@ -5,7 +5,7 @@ public class CoinItemAsset : AssetObject {
     public AssetRef<EntityPrototype> Prefab;
     public FP SpawnChance = FP._0_10, AboveAverageBonus = 0, BelowAverageBonus = 0;
     public SoundEffect BlockSpawnSoundEffect = SoundEffect.World_Block_Powerup;
-    public bool CustomPowerup, LivesOnlyPowerup;
+    public bool BigPowerup, VerticalPowerup, CustomPowerup, LivesOnlyPowerup;
     public bool CanSpawnFromBlock = true;
     public bool OnlyOneCanExist = false;
 
