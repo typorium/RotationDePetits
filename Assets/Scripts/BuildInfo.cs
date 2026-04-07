588178188f8ef117efc6b8e6d5d053d6e180fd75
typorium <lmmsmusic2@gmail.com>
2026-04-07 23:09:05 +0200

    Modified star and coins range in Star Chaser mode

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-03-30T01:46:16.9411198Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-07T20:44:32.7572694Z");
 }
