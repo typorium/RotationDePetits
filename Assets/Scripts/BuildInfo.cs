6aa9ef43d50c22a9aef773353874f86a0a831dc1
Danny A <ipodtouch0218@gmail.com>
2026-04-08 19:20:07 -0400

    nightly v2.1.1.0 (4/8)

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-04T02:31:34.9209272Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-08T22:59:57.8453276Z");
 }
