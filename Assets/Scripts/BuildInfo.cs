b4a60f0624449cd979823ee7ef8c687ae5f9dc9a
typorium <lmmsmusic2@gmail.com>
2026-04-09 01:55:52 +0200

    CoinRunner + RotationDesPieces: Coin for items passé de 1-20 a 1-999

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-08T23:40:24.1852811Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-08T23:49:57.7401515Z");
 }
