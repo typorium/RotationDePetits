09a4627315dc9074240d4ad2efd33246437aa86e
typorium <lmmsmusic2@gmail.com>
2026-04-10 20:51:17 +0200

    Main -> Master

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-08T23:49:57.7401515Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-10T16:23:29.6119510Z");
 }
