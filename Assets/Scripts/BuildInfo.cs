365ffc0aa1ed89b034c315fb9ae8b6482d13ecfc
Ludenbergy <lucaspannocchia@gmail.com>
2026-04-12 19:32:23 +0200

    Map Chateau !!!!!!!!!!

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-11T19:33:25.7657380Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-12T17:31:25.0411100Z");
 }
