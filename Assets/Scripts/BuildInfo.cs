ebd7c9aa8eaf0b9e9c9a2d0d6c26c62506e2b76e
Ludenbergy <lucaspannocchia@gmail.com>
2026-04-17 20:37:36 +0200

    fix maps + début de ciel

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-14T15:12:26.8205570Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-17T18:36:22.4009410Z");
 }
