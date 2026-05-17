8b90319539a4462926d5c1a14b29c0cba9eee32d
typorium <lmmsmusic2@gmail.com>
2026-05-17 21:52:10 +0200

    Fix out of bound error

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-05-17T19:06:40.6352098Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-05-17T19:42:52.0313734Z");
 }
