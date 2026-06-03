3c6a22c4977196bdf56d394b5bd68acdd4b93070
typorium <lmmsmusic2@gmail.com>
2026-06-03 19:57:19 +0200

    Changement de version

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-06-03T17:50:31.6327864Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-06-03T17:54:42.8281232Z");
 }
